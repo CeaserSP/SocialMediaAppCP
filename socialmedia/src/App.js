fd01c6b1dc2b9126fcddb7159f629befb4d131e7
Sheldon Perkins <csperkins18@gmail.com>
2020-02-05 14:08:30 -0500

    app.js setup

--- socialmedia/src/App.js
+++ socialmedia/src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Link, Route, Switch } from 'react-router-dom';
+import withAuth from './withAuth';
+import Home from './Home';
+import Secret from './Secret';
+import Login from './Login';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/secret">Secret</Link></li>
+          <li><Link to="/login">Login</Link></li>
+        </ul>
+
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/secret" component={withAuth(Secret)} />
+          <Route path="/login" component={Login} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
