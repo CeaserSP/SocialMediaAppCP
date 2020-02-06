92a37622f8dadf61c8f0b36c3d0d208a9bb27a52
Sheldon Perkins <csperkins18@gmail.com>
2020-02-05 22:10:40 -0500

    navbar and jumbotron

--- socialmedia/src/App.js
+++ socialmedia/src/App.js
@@ -1,3 +1,6 @@
+import { Button } from 'react-bootstrap';
+import { Navbar } from 'react-bootstrap';
+import { Jumbotron } from 'react-bootstrap';
 import React, { Component } from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
 import withAuth from './withAuth';
@@ -9,8 +12,24 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="/">
+            FAST SYNC
+    </Navbar.Brand>
+        </Navbar>
+        <Jumbotron>
+          <h1>Welcome to Fast Sync!</h1>
+          <p>
+            This is a simple and easy to user oriented Social Media Application. The purpose behind this application is to provide a way to connect with friends, checkout the latest news, or just watch a funny video. There are no likes, follow counts or any influencer driven metrics here.
+  </p>
+          <p>If this sounds like the social media app you've been waiting for <Link to='/login'>Login!</Link> If you are already apart of our community, <Link to='/login'>Login here!</Link></p>
+          <p>
+            <Button variant="primary">Learn more</Button>
+          </p>
+        </Jumbotron>
         <ul>
-          <li><Link to="/">Home</Link></li>
+          <li><Button variant="primary" className="mr-2">Primary</Button>
+            <Link to="/">Home</Link></li>
           <li><Link to="/secret">Secret</Link></li>
           <li><Link to="/login">Login</Link></li>
         </ul>
